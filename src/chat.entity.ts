a797cc3390ac2fb91263bfc1aab19cc1bad32992
jos3duardo <jos3duardolopes@gmail.com>
2022-05-17 23:19:06 -0400

    chat finish

---
+++ src/chat.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Chat {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column({ unique: true })
+  text: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
